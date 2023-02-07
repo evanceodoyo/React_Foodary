852d459c948f8df24706d10e6bf8b323c44dbad2
Evance Odoyo <odoyoevanc@gmail.com>
2023-02-07 20:07:23 +0300

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+      <div className="footer-container">
+         <p className="footer-text">Evance's Foodary © 2023</p>
+      </div>
+    );
+  }
+  export default Footer;
---
+++ src/recipe.js
@@ -0,0 +1,45 @@
+export const my_recipe = {
+    recipe_1: {
+      name: 'Chirashi Don',
+      preparation_time: '30 minutes',
+      servings: '2',
+      ingredients: [
+        '1/2 cup rice',
+        '1/2 cup water',
+        '2 eggs',
+        '300g of salmon, tuna and salmon roe',
+      ],
+      instructions: [
+        'Cook rice and water in a rice cooker',
+        'Fry eggs in a pan',
+        'Serve rice, eggs and fish on a plate',
+      ],
+      image : '/img/recipe1.png',
+   
+    },
+    recipe_2: {
+        name: 'Matoke with Beef',
+        preparation_time: '45 minutes',
+        servings: '5',
+        ingredients: [
+            '12 Matoke (Green bananas)',
+            '2 tablespoons Oil',
+            '1/2 medium onion - diced',
+            '1kg Beef cut into small cubes',
+            '4 medium tomatoes - diced',
+            '1 teaspoon paprika',
+            'Salt and pepper',
+            'Water',
+            'A handful of chopped corriander leaves',
+        ],
+        instructions: [
+            'Peel and cut the matoke in 1.5 inch pieces as you put them in water to avoid discoloration.',
+            "Heath the oil in a pan or a skillet over medium-high heat, add onions, and cook for about two minutes untill soft.",
+            'Stir in the beef and add paprika, salt, and pepper. Mix well, cover, and leave to cook for 10 minutes, stirring halfway.',
+            'Add matoke, diced tomatoes, and water. Stir, cover, and leave to simmer for about 25 to 30 minutes (till the matoke becomes tender).',
+            'Stir in the coriander and serve.',
+        ],
+        image : '/img/recipe2.png',
+    
+    },
+  }
